7ecf1a6d071d14f203a48eafd018ba94b2c791e1
Mundre Sthavir <sthavir@cse.iitb.ac.in>
2023-04-19 18:51:50 +0530

    _bayes_shrink and _visu_shrink added

--- p.py
+++ p.py
@@ -1,13 +1,56 @@
+import math
 import cv2
 import numpy as np
 import pywt
-
+from test import _wavelet_threshold
 def threshold(coeffs,thresh):
     coeffs_thresh = list(coeffs)
     coeffs_thresh[0] = coeffs[0]
     for i in range(1, len(coeffs)):
         coeffs_thresh[i] = pywt.threshold(coeffs[i], thresh, mode='soft')
     return coeffs_thresh
+
+def universal_threshold(coeffs,sigma):
+    
+    coeffs_thresh = list(coeffs)
+    thresh = sigma*math.sqrt(2*math.log(len(coeffs[0])))
+    print(thresh)
+    coeffs_thresh[0] = coeffs[0]
+    for i in range(1, len(coeffs)):
+        coeffs_thresh[i] = pywt.threshold(coeffs[i], thresh, mode='soft')
+    return coeffs_thresh
+
+def mse(img1, img2):
+   h, w , c = img1.shape
+   diff = cv2.subtract(img1, img2)
+   err = np.sum(diff**2)
+   mse = err/(float(h*w*c))
+   return mse
+
+def nlb_threshold(coeffs, sigma, h, p):
+    """
+    Non-local Bayes thresholding function for wavelet coefficients
+    coeffs: list of wavelet coefficients
+    sigma: estimated noise variance
+    h: non-local similarity parameter
+    p: patch size
+    """
+    coeffs_thresh = list(coeffs)
+    coeffs_thresh[0] = coeffs[0]
+    for i in range(1, len(coeffs)):
+        c = coeffs[i]
+        c_thresh = np.zeros_like(c)
+        for j in range(c.shape[0]):
+            for k in range(c.shape[1]):
+                patch = c[max(0, j-p):min(c.shape[0], j+p+1), max(0, k-p):min(c.shape[1], k+p+1)]
+                patch_dists = np.sum((patch - c[j,k])**2, axis=(2,3))
+                patch_weights = np.exp(-patch_dists / (h**2 * sigma))
+                patch_weights /= np.sum(patch_weights)
+                c_thresh[j,k] = np.sum(patch_weights * patch)
+        coeffs_thresh[i] = c_thresh
+    return coeffs_thresh
+
+
 
 def denoise (path,params):
     # Load the noisy image
@@ -21,14 +64,15 @@ def denoise (path,params):
 
     # Apply wavelet transform on the Y channel
     coeffs = pywt.dwt2(y, params[0])
-
+    #sigma = np.median(np.abs(pywt.wavedec2(img, 'db4', mode='symmetric', level=4)[1])) / 0.6745
     # Threshold the high-frequency coefficients
-    coeffs_thresh = threshold(coeffs,params[1])
     
+    coeffs_thresh = threshold(coeffs,params[1])
+    #print(len(coeffs),len(coeffs_thresh))
 
     # Reconstruct the denoised Y channel
     y_denoised = pywt.idwt2(coeffs_thresh, params[0])
-
+    print(y.shape,y_denoised.shape)
     # Merge the channels
     img_denoised_ycrcb = img
     img_denoised_ycrcb[:,:,0] = y_denoised
@@ -39,6 +83,32 @@ def denoise (path,params):
     img_denoised = cv2.cvtColor(img_denoised_ycrcb, cv2.COLOR_YCrCb2BGR)
     return img_denoised
     # Display the denoised image
-cv2.imshow('Denoised image', denoise('Figure_1.png',['db4',20]))
+
+img = cv2.imread('russia_var0.5.jpg')
+# Load the noisy image
+
+
+    # Convert the image to YCbCr color space
+img_ycrcb = cv2.cvtColor(img, cv2.COLOR_BGR2YCrCb)
+
+    # Split the channels
+y, cr, cb = cv2.split(img_ycrcb)
+y_denoised = _wavelet_threshold(image=y,wavelet='db8',method='VisuShrink',sigma=2,wavelet_levels=3)
+cr_denoised = _wavelet_threshold(image=cr,wavelet='db8',method='VisuShrink',sigma=2,wavelet_levels=3)
+cb_denoised = _wavelet_threshold(image=cb,wavelet='db8',method='VisuShrink',sigma=2,wavelet_levels=3)
+img_denoised_ycrcb = img
+img_denoised_ycrcb[:,:,0] = y_denoised
+img_denoised_ycrcb[:,:,1] = cr
+img_denoised_ycrcb[:,:,2] = cb
+img_denoised = cv2.cvtColor(img_denoised_ycrcb, cv2.COLOR_YCrCb2BGR)
+
+
+# applying kernels to the input image to get the sharpened image
+img2 = cv2.imread("russia_org.jpg")
+
+print(mse(img2,img_denoised))
+print(mse(img2,img))
+cv2.imshow('Denoised image', img_denoised.astype(np.uint8))
+
 cv2.waitKey(0)
 cv2.destroyAllWindows()
--- test.py
+++ test.py
@@ -1,50 +1,176 @@
+from logging import warn
 import numpy as np
-from scipy.signal import convolve2d
-import cv2
-def dwt2(image, wavelet='haar', mode='periodization'):
-    """
-    2D Discrete Wavelet Transform using only the dwt function.
+import pywt
+import scipy
 
-    Args:
-        image (ndarray): Input 2D image.
-        wavelet (str, optional): Wavelet to use for the transformation.
-            Default is 'haar'.
-        mode (str, optional): Signal extension mode. Default is 'periodization'.
+def _bayes_thresh(details, var):
+    """BayesShrink threshold for a zero-mean details coeff array."""
+    # Equivalent to:  dvar = np.var(details) for 0-mean details array
+    dvar = np.mean(details*details)
+    eps = np.finfo(details.dtype).eps
+    thresh = var / np.sqrt(max(dvar - var, eps))
+    return thresh
 
-    Returns:
-        (cA, (cH, cV, cD)): Tuple containing the approximation coefficients
-        and the detail coefficients in horizontal, vertical, and diagonal
-        directions, respectively.
+
+def _universal_thresh(img, sigma):
+    """ Universal threshold used by the VisuShrink method """
+    return sigma*np.sqrt(2*np.log(img.size))
+
+def _sigma_est_dwt(detail_coeffs, distribution='Gaussian'):
+    
     """
-    # Decomposition filter and scaling filter for the selected wavelet
-    if wavelet == 'haar':
-        # Haar wavelet filters
-        h0 = np.array([1, 1]) / np.sqrt(2)
-        h1 = np.array([-1, 1]) / np.sqrt(2)
-    elif wavelet == 'db1':
-        # Daubechies-1 wavelet filters
-        h0 = np.array([1, 1]) / np.sqrt(2)
-        h1 = np.array([-1, 1]) / np.sqrt(2)
+    References
+    ----------
+    .. [1] D. L. Donoho and I. M. Johnstone. "Ideal spatial adaptation
+       by wavelet shrinkage." Biometrika 81.3 (1994): 425-455.
+       :DOI:`10.1093/biomet/81.3.425`
+    """
+    # Consider regions with detail coefficients exactly zero to be masked out
+    detail_coeffs = detail_coeffs[np.nonzero(detail_coeffs)]
+
+    if distribution.lower() == 'gaussian':
+        # 75th quantile of the underlying, symmetric noise distribution
+        denom = scipy.stats.norm.ppf(0.75)
+        sigma = np.median(np.abs(detail_coeffs)) / denom
     else:
-        # Unsupported wavelet
-        raise ValueError('Unsupported wavelet: {}'.format(wavelet))
+        raise ValueError("Only Gaussian noise estimation is currently "
+                         "supported")
+    return sigma
 
-    # Convolve rows with the decomposition filter, then downsample
-    cA = convolve2d(image, h0[np.newaxis, :], mode=mode, boundary='symm', axis=1)[:, ::2]
-    cDh = convolve2d(image, h1[np.newaxis, :], mode=mode, boundary='symm', axis=1)[:, ::2]
 
-    # Convolve columns of approximation with the decomposition filter, then downsample
-    cA = convolve2d(cA, h0[:, np.newaxis], mode=mode, boundary='symm', axis=0)[::2, :]
-    cDv = convolve2d(cA, h1[:, np.newaxis], mode=mode, boundary='symm', axis=0)[::2, :]
 
-    # Convolve columns of detail with the decomposition filter, then downsample
-    cDh = convolve2d(cDh, h0[:, np.newaxis], mode=mode, boundary='symm', axis=0)[::2, :]
-    cDd = convolve2d(cDh, h1[:, np.newaxis], mode=mode, boundary='symm', axis=0)[::2, :]
 
-    return (cA, (cDh, cDv, cDd))
+def _wavelet_threshold(image, wavelet, method=None, threshold=None,
+                       sigma=None, mode='soft', wavelet_levels=None):
+    
+    
+    wavelet = pywt.Wavelet(wavelet)
+    if not wavelet.orthogonal:
+        warn(f'Wavelet thresholding was designed for '
+             f'use with orthogonal wavelets. For nonorthogonal '
+             f'wavelets such as {wavelet.name},results are '
+             f'likely to be suboptimal.')
 
-img = cv2.imread('Figure_1.png')
-print(dwt2(image=(img[:,:,0]+img[:,:,1]+img[:,:,2])/3));
-#cv2.imshow('Reconstructed', k)
-#cv2.waitKey(0)
-#cv2.destroyAllWindows()
+    # original_extent is used to workaround PyWavelets issue #80
+    # odd-sized input results in an image with 1 extra sample after waverecn
+    original_extent = tuple(slice(s) for s in image.shape)
+
+    # Determine the number of wavelet decomposition levels
+    if wavelet_levels is None:
+        # Determine the maximum number of possible levels for image
+        wavelet_levels = pywt.dwtn_max_level(image.shape, wavelet)
+        print(image.shape)
+        # Skip coarsest wavelet scales (see Notes in docstring).
+        wavelet_levels = max(wavelet_levels - 3, 1)
+
+    coeffs = pywt.wavedecn(image, wavelet=wavelet, level=wavelet_levels)
+    # Detail coefficients at each decomposition level
+    dcoeffs = coeffs[1:]
+
+    if sigma is None:
+        # Estimate the noise via the method in [2]_
+        detail_coeffs = dcoeffs[-1]['d' * image.ndim]
+        sigma = _sigma_est_dwt(detail_coeffs, distribution='Gaussian')
+
+    if method is not None and threshold is not None:
+        warn(f'Thresholding method {method} selected. The '
+             f'user-specified threshold will be ignored.')
+
+    if threshold is None:
+        var = sigma**2
+        if method is None:
+            raise ValueError(
+                "If method is None, a threshold must be provided.")
+        elif method == "BayesShrink":
+            # The BayesShrink thresholds from [1]_ in docstring
+            threshold = [{key: _bayes_thresh(level[key], var) for key in level}
+                         for level in dcoeffs]
+        elif method == "VisuShrink":
+            # The VisuShrink thresholds from [2]_ in docstring
+            threshold = _universal_thresh(image, sigma)
+        else:
+            raise ValueError(f'Unrecognized method: {method}')
+
+    if np.isscalar(threshold):
+        # A single threshold for all coefficient arrays
+        denoised_detail = [{key: pywt.threshold(level[key],
+                                                value=threshold,
+                                                mode=mode) for key in level}
+                           for level in dcoeffs]
+    else:
+        # Dict of unique threshold coefficients for each detail coeff. array
+        denoised_detail = [{key: pywt.threshold(level[key],
+                                                value=thresh[key],
+                                                mode=mode) for key in level}
+                           for thresh, level in zip(threshold, dcoeffs)]
+    denoised_coeffs = [coeffs[0]] + denoised_detail
+    return pywt.waverecn(denoised_coeffs, wavelet)[original_extent]
+
+def denoise_wavelet(image, sigma=None, wavelet='db1', mode='soft',
+                    wavelet_levels=None,
+                    convert2ycbcr=True, method='BayesShrink',
+                    rescale_sigma=True, *, channel_axis=None):
+    """
+    
+    References
+    ----------
+    .. [1] Chang, S. Grace, Bin Yu, and Martin Vetterli. "Adaptive wavelet
+           thresholding for image denoising and compression." Image Processing,
+           IEEE Transactions on 9.9 (2000): 1532-1546.
+           :DOI:`10.1109/83.862633`
+    .. [2] D. L. Donoho and I. M. Johnstone. "Ideal spatial adaptation
+           by wavelet shrinkage." Biometrika 81.3 (1994): 425-455.
+           :DOI:`10.1093/biomet/81.3.425`
+    
+    """
+    multichannel = channel_axis is not None
+    if method not in ["BayesShrink", "VisuShrink"]:
+        raise ValueError(f'Invalid method: {method}. The currently supported '
+                         f'methods are "BayesShrink" and "VisuShrink".')
+
+    # floating-point inputs are not rescaled, so don't clip their output.
+    clip_output = image.dtype.kind != 'f'
+
+    if convert2ycbcr and not multichannel:
+        raise ValueError("convert2ycbcr requires channel_axis to be set")
+
+    image, sigma = _scale_sigma_and_image_consistently(image,
+                                                       sigma,
+                                                       multichannel,
+                                                       rescale_sigma)
+
+    if convert2ycbcr:
+        out = color.rgb2ycbcr(image)
+        # convert user-supplied sigmas to the new colorspace as well
+        if rescale_sigma:
+            sigma = _rescale_sigma_rgb2ycbcr(sigma)
+        for i in range(3):
+            # renormalizing this color channel to live in [0, 1]
+            _min, _max = out[..., i].min(), out[..., i].max()
+            scale_factor = _max - _min
+            if scale_factor == 0:
+                # skip any channel containing only zeros!
+                continue
+            channel = out[..., i] - _min
+            channel /= scale_factor
+            sigma_channel = sigma[i]
+            if sigma_channel is not None:
+                sigma_channel /= scale_factor
+            out[..., i] = denoise_wavelet(channel,
+                                          wavelet=wavelet,
+                                          method=method,
+                                          sigma=sigma_channel,
+                                          mode=mode,
+                                          wavelet_levels=wavelet_levels,
+                                          rescale_sigma=rescale_sigma)
+            out[..., i] = out[..., i] * scale_factor
+            out[..., i] += _min
+        out = color.ycbcr2rgb(out)
+    
+
+
+    if clip_output:
+        clip_range = (-1, 1) if image.min() < 0 else (0, 1)
+        out = np.clip(out, *clip_range, out=out)
+    return out
+
