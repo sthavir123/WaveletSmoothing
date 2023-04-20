2ca68135e076ac35c1baba8c4187bbe70ea04a68
Mundre Sthavir <sthavir@cse.iitb.ac.in>
2023-04-20 17:50:35 +0530

    chnages

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import numpy as np
 import pywt
 from wavelet_thresholding import _wavelet_threshold
 
-def main(path,wavelet='db4',method='BiasShrink',levels=1,sigma=None):
+def main(path,wavelet='db4',method='BiasShrink',levels=1,sigma=None,mode = 'soft'):
     # Load the noisy image
     img = cv2.imread(path)
     
@@ -19,9 +19,9 @@ def main(path,wavelet='db4',method='BiasShrink',levels=1,sigma=None):
     # Split the channels
     y, cr, cb = cv2.split(img_ycrcb)
     # apply wavelet threshold on each channel seperately
-    y_denoised  = _wavelet_threshold(image=y, wavelet=wavelet,method=method,wavelet_levels=levels)
-    cr_denoised = _wavelet_threshold(image=cr,wavelet=wavelet,method=method,wavelet_levels=levels)
-    cb_denoised = _wavelet_threshold(image=cb,wavelet=wavelet,method=method,wavelet_levels=levels)
+    y_denoised  = _wavelet_threshold(image=y, wavelet=wavelet,method=method,wavelet_levels=levels,mode=mode)
+    cr_denoised = _wavelet_threshold(image=cr,wavelet=wavelet,method=method,wavelet_levels=levels,mode=mode)
+    cb_denoised = _wavelet_threshold(image=cb,wavelet=wavelet,method=method,wavelet_levels=levels,mode=mode)
     
     #recontruct the image in YCrcCb color space
     img_denoised_ycrcb = np.zeros_like(img)
